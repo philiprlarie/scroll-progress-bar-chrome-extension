2d685a34f634dc2d59734238a907a9141fb08f4c
philiprlarie <philiprlarie@gmail.com>
2017-07-30 10:34:44 -0700

    Get the scroll progress bar working

---
+++ content.js
@@ -0,0 +1,14 @@
+var scrollProgressBar = document.createElement('div');
+// scrollProgressBar.innerHTML = '<div></div>';
+scrollProgressBar.setAttribute('id', 'scroll-progress-bar-chrome-extension');
+document.body.appendChild(scrollProgressBar);
+
+window.addEventListener('scroll', function () {
+  var percentScrolled;
+  if (document.body.offsetHeight <= window.innerHeight) {
+    percentScrolled = 0;
+  } else {
+    percentScrolled = document.body.scrollTop / (document.body.offsetHeight - window.innerHeight) * 100;
+  }
+  scrollProgressBar.style.width = percentScrolled + '%';
+});
